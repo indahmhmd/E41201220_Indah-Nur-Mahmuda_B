744ab29e095d218e46fa3f6950eafdbabf857d30
indahmhmd <indahnurmahmuda17@gmail.com>
2021-03-16 21:01:24 +0700

    Create Variableproblem.java

---
+++ Minggu 2/Variableproblem/src/variableproblem/Variableproblem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableproblem;
+
+
+public class Variableproblem {
+
+    
+    public static void main(String[] args) {
+        //deklarasi variable yang tidak benar
+        int 2beOrNotbe;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+        
+        //deklarasi variable yang benar
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number;
+        
+        
+    }
+    
+}
