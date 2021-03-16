8a1b4c825b24020ddc22cc1fd888a651d6779089
indahmhmd <indahnurmahmuda17@gmail.com>
2021-03-16 18:26:18 +0700

    Create Latihan_b.java

---
+++ Minggu 2/Latihan_b/src/latihan_b/Latihan_b.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_b;
+
+/**
+ *
+ * @author L E N O V O
+ */
+public class Latihan_b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println (nama);
+        System.out.println (a);
+    }
+    
+}
