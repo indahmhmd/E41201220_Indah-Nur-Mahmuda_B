17a0217fd49d16beb8b663d1a5ffb5bc15944fc5
indahmhmd <indahnurmahmuda17@gmail.com>
2021-03-23 18:46:44 +0700

    Create Tugas_a.java

---
+++ Minggu 3/tugas_a/src/tugas_a/Tugas_a.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_a;
+
+/**
+ *
+ * @author L E N O V O
+ */
+public class Tugas_a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        String[] teks = {"Indah", "Nur", "Mahmuda"};
+        
+        
+        for(int j=0; j<teks.length; j++) {
+            System.out.print(teks[j]);
+            System.out.print(" ");
+        }
+    }
+    
+}
