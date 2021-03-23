e06b95c9f938b8e8c152437451b5e6c7864dd472
indahmhmd <indahnurmahmuda17@gmail.com>
2021-03-23 18:47:04 +0700

    Create Tugas_b.java

---
+++ Minggu 3/tugas_b/src/tugas_b/Tugas_b.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_b;
+
+/**
+ *
+ * @author L E N O V O
+ */
+public class Tugas_b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] angka =  {9, 16, 21, 5, 2};
+        
+        for(int i=0; i<angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+        
+    }
+    
+}
