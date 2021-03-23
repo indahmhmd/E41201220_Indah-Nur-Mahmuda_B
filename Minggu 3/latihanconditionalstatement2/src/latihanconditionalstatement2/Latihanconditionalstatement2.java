d4dd394eb0114dd22531e92510a54a2229e624e0
indahmhmd <indahnurmahmuda17@gmail.com>
2021-03-23 07:04:48 +0700

    Create Latihanconditionalstatement2.java

---
+++ Minggu 3/latihanconditionalstatement2/src/latihanconditionalstatement2/Latihanconditionalstatement2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanconditionalstatement2;
+
+import java.util.Scanner;
+public class Latihanconditionalstatement2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String lampu;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Inputkan nama warna: ");
+        lampu = scan.nextLine();
+        
+        switch (lampu) {
+            case "merah":
+                System.out.println("Lampu merah, berhenti!");
+                break;
+            case "kuning":
+                System.out.println("Lampu kuning, harap hati-hati");
+                break;
+            case "hijau":
+                System.out.println("Lampu hijau, silahkan jalan");
+                break;
+            default:
+                System.out.println("Warna lampu salah!");
+        }
+    }
+    
+}
