e558c44cd4f977d9c4413a7a26e887932f0d2113
indahmhmd <indahnurmahmuda17@gmail.com>
2021-04-10 19:01:03 +0700

    Create Latihanm4d.java

---
+++ minggu 4/Latihanm4d/src/latihanm4d/Latihanm4d.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanm4d;
+
+// @indah
+public class Latihanm4d {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int angka[] = {3,1,42,24,12};
+        
+        for( int x : angka ) {
+            System.out.print(x + " ");
+        }
+    
+    }
+    
+}
