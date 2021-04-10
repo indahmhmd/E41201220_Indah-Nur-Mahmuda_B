e03e12a98970a44f64b3ebfebf654b318ca89b88
indahmhmd <indahnurmahmuda17@gmail.com>
2021-04-10 19:00:01 +0700

    Create Latihanm4a.java

---
+++ minggu 4/latihanm4a/src/latihanm4a/Latihanm4a.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanm4a;
+
+// @indah
+public class Latihanm4a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 10) {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+    }
+    
+}
